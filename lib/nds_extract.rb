cf971ef605faa7ab33eb788d2ed10ae17d621353
Alex Bonwit <alexbonwit@mac.com>
2019-11-12 01:30:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -112,11 +112,7 @@ def movies_with_directors_set(source)
   # Array of arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
-
-  
   movie_directors = []
-  
-  
   
   counter = 0
   
